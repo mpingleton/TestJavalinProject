41c19204cd805528d223b95559d95a115d50f8f6
Michael Pingleton <mnpingleton@gmail.com>
2023-02-17 17:13:26 -0500

    Added state example.

--- src/main/java/TestJavalinProject/GetAllUsers.java
+++ src/main/java/TestJavalinProject/GetAllUsers.java
@@ -4,9 +4,15 @@ import io.javalin.http.Context;
 import io.javalin.http.Handler;
 
 public class GetAllUsers implements Handler {
+    
+    int i = 0;
+    
+    public GetAllUsers(int l) {
+        i = l;
+    }
 
     @Override
     public void handle(Context cntxt) throws Exception {
-        
+        cntxt.result("i = " + i);
     }
 }
