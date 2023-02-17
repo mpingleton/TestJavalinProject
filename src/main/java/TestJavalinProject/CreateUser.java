13fc486790b355db6ae5c681d8c091310976a3f1
Michael Pingleton <mnpingleton@gmail.com>
2023-02-17 16:34:03 -0500

    Initial commit.

---
+++ src/main/java/TestJavalinProject/CreateUser.java
@@ -0,0 +1,10 @@
+package TestJavalinProject;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
+public class CreateUser implements Handler {
+
+    @Override
+    public void handle(Context cntxt) throws Exception {}
+}
---
+++ src/main/java/TestJavalinProject/GetAllUsers.java
@@ -0,0 +1,12 @@
+package TestJavalinProject;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
+public class GetAllUsers implements Handler {
+
+    @Override
+    public void handle(Context cntxt) throws Exception {
+        
+    }
+}
