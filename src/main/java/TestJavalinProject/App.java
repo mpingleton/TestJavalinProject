0ed9c8e4a9d6e5042e94e0aeeff747216963b517
Michael Pingleton <mnpingleton@gmail.com>
2023-02-17 16:45:59 -0500

    Added state example.

--- src/main/java/TestJavalinProject/App.java
+++ src/main/java/TestJavalinProject/App.java
@@ -9,13 +9,15 @@ import static io.javalin.apibuilder.ApiBuilder.*;
 public class App {
 
     public static void main(String[] args) {
+        int i = 12;
+        
         // Create a Javalin instance.
         var app = Javalin.create();
         
         // Set up the Javalin handlers.
         app.routes(() -> {
             path("users", () -> {
-                get(new GetAllUsers());
+                get(new GetAllUsers(i));
                 put(new CreateUser());
             });
         });
